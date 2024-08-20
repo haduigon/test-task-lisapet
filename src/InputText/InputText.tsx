57a683e8c0ab37c72ec9f7c94da06dafc203bb57
haduigon <haduigon@gmail.com>
2024-08-20 12:49:32 +0300

    aaded a lot of styles

--- src/InputText/InputText.tsx
+++ src/InputText/InputText.tsx
@@ -1,29 +1,41 @@
-import styles from './InputText.module.scss';
+import styles from "./InputText.module.scss";
+import searchIconGrey from "../assets/icons/searchIconGrey.png";
+import questionIconGrey from "../assets/icons/questionIconGrey.png";
 
 const InputText = () => {
-
-  // for (var i = 0; i < 5; i++) {
-  //   setTimeout(() => {
-  //     console.log(i)
-  //   }, 1000)
-  // }
   return (
-    <div
-      className={styles.box}
-      tabIndex={0}
-    >
-      <i>|</i>
+    <div className={styles.box} tabIndex={0}>
+
+      
+      <div className={`centered-flex ${styles.innerBox}`}> 
+
+        <div className="centered-flex">
+        <img
+          src={searchIconGrey}
+          className={styles.searchIcon}
+          alt="search-icon"
+        />
+      </div>
+
       <input
-        type='text'
+        type="text"
         placeholder="Input..."
-        className={styles.text}
-      />
-      <div className={styles.iconTextBox}>
+        className={`${styles.text}`}
+        />
+
+      <div className="centered-flex ml-5">
+        <img src={questionIconGrey} className={styles.searchIcon}/>
+        <div className={`${styles.iconTextBox}`}>
         <span className={styles.iconText}>⌘</span>
         <span className={styles.iconText}>K</span>
       </div>
+        </div>
+        
+      </div>
+
+
     </div>
-  )
-}
+  );
+};
 
 export default InputText;
